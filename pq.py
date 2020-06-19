54d81b749984bff26b9dfe8c254c7afbbf946f45
Anna Peery <avpeery@gmail.com>
2020-06-19 09:58:24 -0700

    Improved test cases with unit test implementation

--- pq.py
+++ pq.py
@@ -1,3 +1,5 @@
+import unittest
+
 class Item:
     def __init__(self, val, priority):
         self.val = val
@@ -20,21 +22,17 @@ class PriorityQueue:
         self.storage = []
 
     def __str__(self):
-        return "The first item in this list is", self.storage[0].item, ". There are ", len(self.storage), "items in this list."
+        return "The first item entered in this list is " + str(self.peek()) + ". There are " + str(len(self.storage)) + " items in this list."
 
     def insert(self, val, priority):
         to_insert = Item(val, priority)
 
         insert_index = 0
         while insert_index < len(self.storage):
-            if self.storage[insert_index] < to_insert:
+            if self.storage[insert_index] == to_insert or self.storage[insert_index] < to_insert:
                 break
-            
-            if self.storage[insert_index] == to_insert:
-                raise  DuplicatePriorityException(to_insert)
-
             insert_index += 1
-
+        
         self.storage.insert(insert_index, to_insert)
 
     def peek(self):
@@ -58,29 +56,53 @@ class EmptyQueueException(Exception):
     def __str__(self):
         return f'{self.expression} -> {self.message}'
 
-class DuplicatePriorityException(Exception):
-    '''Raised when there are duplicate priority values'''
+#Handling duplicate priority now
+# class DuplicatePriorityException(Exception):
+#     '''Raised when there are duplicate priority values'''
 
-    def __init__(self, expression, message='Duplicate priority values'):
-        self.expression = expression
-        self.message = message
-        super().__init__(self.message)
+#     def __init__(self, expression, message='Duplicate priority values'):
+#         self.expression = expression
+#         self.message = message
+#         super().__init__(self.message)
     
-    def __str__(self):
-        return f'{self.expression} -> {self.message}'
+#     def __str__(self):
+#         return f'{self.expression} -> {self.message}'
 
-def simple_test():
-    print("Running simple test...")
-    pq = PriorityQueue()
-    pq.insert("c", 3)
-    pq.insert("a", 1)
-    pq.insert("b", 2)
+class Test(unittest.TestCase):
+    def testSimple(self):
+        pq = PriorityQueue()
+        pq.insert("c", 3)
+        pq.insert("a", 1)
+        pq.insert("b", 2)
+        pq.insert("c", 1)
+        pq.insert("3", 2)
+        pq.insert("GEORGE", 1)
 
-    assert(pq.pop() == "a")
-    assert(pq.pop() == "b")
-    assert(pq.pop() == "c")
-    print("Simple test passed!")
+        assert(pq.pop() == "a")
+        assert(pq.pop() == "c")
+        assert(pq.pop() == "GEORGE")
+        assert(pq.pop() == "b")
+        assert(pq.pop() == "3")
+        assert(pq.pop() == "c")
+        print("Simple test passed!")
+
+    def testEmpty(self):
+        with self.assertRaises(EmptyQueueException):
+            pq = PriorityQueue()
+            pq.pop()
+        print("Empty queue test passed!")
+    
+    def testFloatsAndNegatives(self):
+        pq = PriorityQueue()
+        pq.insert("c", 1)
+        pq.insert("e", 1.01)
+        pq.insert("first", -1)
+
+        assert(pq.pop() == "first")
+        assert(pq.pop() == "c")
+        assert(pq.pop() == "e")
+        print("Float test passed!")
 
 
 if __name__ == "__main__":
-    simple_test()
+    unittest.main()
